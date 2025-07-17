db145e6328f5b88f5a7fe708116deeb37f8dcf9c
GabrielHidaN <victorbielgv7@gmail.com>
2025-07-17 19:14:53 -0300

    :lock:feat add TaskModel Class

---
+++ src/main/java/com/hidan/todo_list/task/TaskModel.java
@@ -0,0 +1,45 @@
+package com.hidan.todo_list.task;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+    /*
+    Id
+    user(id)
+    description
+    tittle
+    date of initial
+    date of end
+    priority
+     */
+
+
+@Data
+@Entity(name = "tb_task")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String description;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+
+}
